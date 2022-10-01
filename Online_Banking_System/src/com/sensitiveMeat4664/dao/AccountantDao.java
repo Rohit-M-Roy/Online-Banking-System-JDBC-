399b6a476c221cea853b4f3845167e223e0e7a8b
Rohit Roy <mrrohitroy09@gmail.com>
2022-10-01 13:55:46 +0530

    accountant dao impl

--- Online_Banking_System/src/com/sensitiveMeat4664/dao/AccountantDao.java
+++ Online_Banking_System/src/com/sensitiveMeat4664/dao/AccountantDao.java
@@ -1,11 +1,20 @@
 package com.sensitiveMeat4664.dao;
 
+import java.util.List;
+
+import javax.security.auth.login.AccountException;
+
+import com.sensitiveMeat4664.bean.Account;
+import com.sensitiveMeat4664.bean.AccountDetails;
 import com.sensitiveMeat4664.bean.Customer;
 
 public interface AccountantDao {
 	
 	public boolean accountantLogin(int accountantId);
-	public Customer addCustomer(String fName, String lName, long mobile, String email, int zipcode, String state, String address);
+	public String addCustomer(Customer customer);
+	public boolean removeAccount(int accountId);
+	public AccountDetails veiwAccount (int accountId) throws AccountException;
+	public List<Account> viewAllAccounts() throws AccountException;
 	
 	
 
